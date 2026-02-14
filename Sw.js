a18568eeff0f3749d1a9e9c3ded4dc621b50b936
takojump <rainterior@gmail.com>
2026-02-14 10:51:25 +0700

    Create Sw.js

---
+++ Sw.js
@@ -0,0 +1,20 @@
+const CACHE = "dealchecker-cache-v1";
+const ASSETS = ["./","./index.html","./styles.css","./app.js","./manifest.json"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k === CACHE ? null : caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
